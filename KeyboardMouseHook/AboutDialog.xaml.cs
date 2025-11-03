87dfec80aa738a080f765607a933288244111622
RainbowWolfer <rainbowwolfer@outlook.com>
2025-11-03 23:05:53 +0800

    Update

---
+++ KeyboardMouseHook/AboutDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using RW.Common.WPF.Extensions;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace KeyboardMouseHook;
+
+public partial class AboutDialog : Window {
+	private static AboutDialog? instance;
+
+	public static void ShowInstance() {
+		instance ??= new();
+		instance.ShowAndActivate();
+	}
+
+	public AboutDialog() {
+		InitializeComponent();
+		Title = App.AppName;
+		Closed += AboutDialog_Closed;
+	}
+
+	private void AboutDialog_Closed(object? sender, EventArgs e) {
+		instance = null;
+	}
+
+	private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e) {
+		e.Handled = true;
+
+		try {
+			Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+		} catch (Exception ex) {
+			Debug.WriteLine(ex);
+		}
+	}
+}
